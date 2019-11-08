2c7285673206ef3034bc7305a88387a75118ded6
Droid43 <578738182@qq.com>
2019-11-09 03:30:48 +0800

    [M]fix

--- app/router.ts
+++ app/router.ts
@@ -4,5 +4,6 @@ export default (app: Application) => {
   const { controller, router } = app;
   const api = controller.api;
   router.get('/', controller.home.index);
+  router.get('/api', controller.home.index);
   router.get('/api/wechat/identify', api.wechat.identify.index);
 };
